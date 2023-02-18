df9a5379bfe4d8565e044c4efe9ced01df0b4d14
KvaskovSS <sava0980@yandex.ru>
2023-02-18 17:11:04 +0300

    task 4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def task1() :
     num = int(input('Введите 3-х значное число : '))
-    if num < 100 or num > 999 :
+    if len(str(num)) != 3 :
         print('Число должно быть 3-х значным')
         return
     sum = 0
@@ -9,6 +9,7 @@ def task1() :
         num //= 10
         sum += temp
     print(sum)
+
 
 def task2() :
     ## x - pet x - serg 4x - k найти чему равен х  6x = s x = s/6
@@ -23,5 +24,42 @@ def task2() :
 
     print(f"Серёжа сделал - {Seryozha} \n Петя сделал -  {Petya} \n Катя сделала -  {Katya}" )
 
+
+# 123 | 456 
+def task3() :
+    ticket = int(input("Введите номер билетика :"))
+    if len(str(ticket)) != 6 :
+        print("Номер билетика должен быть шестизначным числом") 
+        return
+    sumRight = 0
+    sumLeft = 0
+    i = 0
+    while ticket != 0 :
+        if i < 3 :
+            sumRight += ticket % 10
+            ticket //= 10
+        else :
+            sumLeft += ticket % 10
+            ticket //= 10
+        i += 1
+    if sumRight == sumLeft :
+        print("YES")
+    else :
+        print("NO")
+
+def task4() :
+    print("У нас имеется шоколадка m на n и мы будем определять модно ли от неё отрезать k долек по прямой")
+    n,m,k = int(input("Введите n : ")),int(input("Введите m : ")),int(input("Введите k : "))
+    if n * m > k:
+        if k % n == 0 or k % m == 0:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        print('NO')
+
 #task1()
-task2()
+#task2()
+#task3()
+#task4()
+
