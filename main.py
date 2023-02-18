00a0b48403422c2b2e3d414e9a71680aa47259a4
KvaskovSS <sava0980@yandex.ru>
2023-02-18 16:42:10 +0300

    task 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def task1(num) :
+def task1() :
+    num = int(input('Введите 3-х значное число : '))
     if num < 100 or num > 999 :
         print('Число должно быть 3-х значным')
         return
@@ -9,9 +10,18 @@ def task1(num) :
         sum += temp
     print(sum)
 
+def task2() :
+    ## x - pet x - serg 4x - k найти чему равен х  6x = s x = s/6
+    S = int(input('Введите число журавликов : '))
+    if S % 6 != 0 :
+        print("Число журавликов должно быть кратно 6 (иначе получаются знаменитые полтора землекопа")
+        return
+    x = S // 6
+    Petya = x
+    Seryozha = x
+    Katya = 4 * x
 
+    print(f"Серёжа сделал - {Seryozha} \n Петя сделал -  {Petya} \n Катя сделала -  {Katya}" )
 
-
-
-num = int(input('Введите 3-х значное число : '))
-task1(num)
+#task1()
+task2()
