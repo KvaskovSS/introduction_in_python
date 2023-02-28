04bcbfd83708ab8352f7eea6c177ec6180680cba
KvaskovSS <sava0980@yandex.ru>
2023-02-28 20:34:00 +0300

    HomeWokr seminar №3

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
+import re
+def isCyrillic(text):
+	return bool(re.search('[а-яА-Я]', text))
+
 def task1() :
     n = int(input("Введите длину массива N :"))
-    arr = [i for i in range(n)]
+
+    list = []
+    list.append(1)
+    print("Заполните массив : ")
+    for i in range(n - 1) :
+        list.append(int(input()))
+    print(list)
     x = int(input("Введите искомое число :"))
     count = 0
-    for i in arr :
-        if arr[i] == x :
+    for i in range(n):
+        if list[i] == x :
             count += 1
-    print(f"{count}")
-
-
-
+    print(count)
 
 def task2() :
     n = int(input("Введите длину массива N :"))
-    arr = [i for i in range(n)]
+    list = []
+    list.append(1)
+    print("Заполните массив : ")
+    for i in range(n - 1) :
+        list.append(int(input()))
+    print(list)
     x = int(input("Введите искомое число :"))
-    res = arr[0]
-    for i in arr:
-        if abs(i - x) < abs(res - x):
-            res = i
-    print(res)
+    
+    min = abs(x - list[0])
+    index = 0
+    for i in range(1, n):
+        count = abs(x - list[i])
+        if count < min:
+            min = count
+            index = i
+    print(list[index])
 
 def task3() :
     eng = {1:'AEIOULNSTR',
-            2:'DG',
-      	    3:'BCMP',
-      	    4:'FHVWY',
-      	    5:'K',
-      	    8:'JZ',
-      	    10:'QZ'}
+      	2:'DG',
+      	3:'BCMP',
+      	4:'FHVWY',
+      	5:'K',
+      	8:'JZ',
+      	10:'QZ'}
     rus = {1:'АВЕИНОРСТ',
-          	2:'ДКЛМПУ',
-          	3:'БГЁЬЯ',
-      	    4:'ЙЫ',
-      	    5:'ЖЗХЦЧ',
-      	    8:'ШЭЮ',
-          	10:'ФЩЪ'}
+      	2:'ДКЛМПУ',
+      	3:'БГЁЬЯ',
+      	4:'ЙЫ',
+      	5:'ЖЗХЦЧ',
+      	8:'ШЭЮ',
+      	10:'ФЩЪ'}
+    text = input("Введите слово : ").upper()
+    if isCyrillic(text) :
+        print(sum([k for i in text for k, v in rus.items() if i in v]))
+    else :
+	    print(sum([k for i in text for k, v in eng.items() if i in v]))
     
-    n = abs(int(input('Введите 1, если играем в английской раскладке, либо 0, если в русской: ')))
-    word = input('Введите слово: ').upper()
-    if n == 1 :
-	    print('За это слово вы получаете', sum([k for i in word for k, v in eng.items() if i in v]), 'очков')
-    elif n == 0 :
-    	print('За это слово вы получаете', sum([k for i in word for k, v in rus.items() if i in v]), 'очков')
 def main() :
     i = int(input("Выберите номер задание [1 ; 3]  : "))
     if i == 1 :
@@ -50,7 +65,7 @@ def main() :
     elif i == 2 :
         task2()
     elif i == 3 :
-        task3()
+       task3()
     #elif i == 4 :
         #task4()
 
